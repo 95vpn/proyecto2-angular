bf1bc563553e3e7d1f689fba72b9feabc61bb74d
Johnny Herrera <jmjaramillo95@gmail.com>
2025-04-08 23:12:20 -0500

    guard

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,15 +4,22 @@ import { AtributoComponent } from "./atributo/atributo.component";
 import { EstructuralComponent } from "./estructural/estructural.component";
 import { PipesComponent } from "./pipes/pipes.component";
 import { ListadoComponent } from "./compra/listado/listado.component";
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AtributoComponent, EstructuralComponent, PipesComponent, ListadoComponent, RouterModule],
+  imports: [RouterOutlet, AtributoComponent, EstructuralComponent, PipesComponent, ListadoComponent, RouterModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'proyecto2';
+
+  constructor() {
+    if (typeof window !== 'undefined' && typeof localStorage !== 'undefined') {
+      localStorage.setItem('id', '1234');
+    }
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,15 @@ import { EstructuralComponent } from './estructural/estructural.component';
 import { ListadoComponent } from './compra/listado/listado.component';
 import { NuevaCompraComponent } from './compra/nueva-compra/nueva-compra.component';
 import { HistorialCompraComponent } from './compra/historial-compra/historial-compra.component';
+import { authGuard } from './auth.guard';
 
 export const routes: Routes = [
+    {path: '', redirectTo: 'pipes', pathMatch: 'full'},
     {path: 'pipes', component: PipesComponent},
     {path: 'estructural', component: EstructuralComponent},
-    {path: 'compra', component: ListadoComponent, children: [
+    {path: 'compra', component: ListadoComponent,
+        canActivate: [authGuard],
+        children: [
         {path: 'nueva', component: NuevaCompraComponent},
         {path: 'historial', component: HistorialCompraComponent}
     ]}
