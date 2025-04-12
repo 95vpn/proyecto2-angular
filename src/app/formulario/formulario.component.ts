0ba7f063264e4f87345db7c4ef8021967a426382
Johnny Herrera <jmjaramillo95@gmail.com>
2025-04-11 22:49:24 -0500

    form

--- src/app/formulario/formulario.component.ts
+++ src/app/formulario/formulario.component.ts
@@ -1,17 +1,21 @@
-import { CommonModule, JsonPipe } from '@angular/common';
+import { AsyncPipe, CommonModule, JsonPipe } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormGroup, FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-formulario',
   standalone: true,
-  imports: [ReactiveFormsModule, CommonModule, JsonPipe],
+  imports: [ReactiveFormsModule, CommonModule, JsonPipe, AsyncPipe],
   templateUrl: './formulario.component.html',
   styleUrl: './formulario.component.scss'
 })
 export class FormularioComponent {
 
-  name = new FormControl('', [Validators.minLength(4), Validators.required]);
+  form = new FormGroup({
+    name : new FormControl('', [Validators.minLength(4), Validators.required]),
+    isChecked : new FormControl(false)
+  });
+  
 
-  isChecked = new FormControl(false);
+  
 }
