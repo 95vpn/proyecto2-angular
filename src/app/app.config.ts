d52fa743dba0ef9b40986b06a341db3c31d816bc
Johnny Herrera <jmjaramillo95@gmail.com>
2025-04-16 22:03:11 -0500

    interceptor

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,8 +3,15 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { InterceptorService } from './service/interceptor.service';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration(), importProvidersFrom(HttpClientModule)]
+  providers: [
+    provideRouter(routes), 
+    provideClientHydration(), 
+    importProvidersFrom(HttpClientModule),
+    {provide: HTTP_INTERCEPTORS, useClass: InterceptorService, multi: true}
+
+  ]
 };
